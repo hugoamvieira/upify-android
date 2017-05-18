23e81ab167580fd5e9391c71e03bc2b61269c248
spncode <hen.spn@gmail.com>
2017-05-18 01:31:15 +0100

    Started Design of Rate Popup Window

--- app/src/main/java/a029589/ismai/pt/upify/CustomCardGrid.java
+++ app/src/main/java/a029589/ismai/pt/upify/CustomCardGrid.java
@@ -1,9 +1,15 @@
 package a029589.ismai.pt.upify;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
+import android.widget.PopupWindow;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +25,10 @@ public class CustomCardGrid extends Card {
 
     protected TextView mTitle;
     protected Button mRate;
+    private  CheckedTextView socialCheckedTextPop;
+    private CheckBox friendlyCheckBoxPop;
+    private PopupWindow popupWindow;
+    private LayoutInflater layoutInflater;
     /**
      * Constructor with a custom inner layout
      * @param context
@@ -51,14 +61,55 @@ public class CustomCardGrid extends Card {
                 Toast.makeText(getContext(), "Click Listener card=", Toast.LENGTH_LONG).show();
             }
         });
+
+
     }
 
     @Override
-    public void setupInnerViewElements(ViewGroup parent, View view) {
+    public void setupInnerViewElements(final ViewGroup parent, View view) {
 
         //Retrieve elements
         mTitle = (TextView) parent.findViewById(R.id.card_main_inner_simple_title);
         mRate = (Button) parent.findViewById(R.id.ratebutton);
+
+        mRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "hey", Toast.LENGTH_LONG).show();
+                layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                ViewGroup container = (ViewGroup)layoutInflater.inflate(R.layout.rate_popup,null);
+                //container.setAlpha(0.8f);
+                container.findViewById(R.id.backgroundpop).setAlpha(0.8f);
+                //socialCheckedTextPop =(CheckedTextView)container.findViewById(R.id.so);
+                friendlyCheckBoxPop = (CheckBox)container.findViewById(R.id.friendlycheckBox);
+
+                friendlyCheckBoxPop.setOnClickListener( new View.OnClickListener(){
+                    @Override
+                    public  void onClick(View view ){
+
+
+
+
+
+
+                    }
+                });
+
+               /* socialCheckedTextPop.setOnClickListener( new View.OnClickListener(){
+                    @Override
+                    public  void onClick(View view ){
+                        if ( socialCheckedTextPop.isChecked()){
+                            socialCheckedTextPop.setChecked(false);
+                        }else{
+                            socialCheckedTextPop.setChecked(true);
+                        }
+
+                    }
+                });*/
+                popupWindow = new PopupWindow(container, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, true);
+                popupWindow.showAtLocation(parent, Gravity.NO_GRAVITY,ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+            }
+        });
 
         if (mTitle!=null)
             mTitle.setText("Zulmira");
